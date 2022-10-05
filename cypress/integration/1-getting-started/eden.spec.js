2b14ff8b146bbb53d3b3b10ee808423612a610f7
Mauro <valentinviale@gmail.com>
2022-10-05 00:38:17 -0300

    se agrego test de servicios

---
+++ cypress/integration/1-getting-started/eden.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+import EdenPage from "../pages/eden.js";
+
+describe("Test de pagina Eden", () => {
+  it("Llamada a servicio: /inicio", () => {
+    cy.callService("inicio", "espectaculos");
+  });
+
+  it("Llamada a servicio: /puntosdeventas", () => {
+    cy.callService(
+      "puntosdeventas",
+      "puntosdeventas",
+      "puntosdeventa_sch.json"
+    );
+  });
+
+  it.only("Verificar las cards de los espectáculos", () => {
+    const eden = new EdenPage();
+
+    cy.visit("https://www.edenentradas.com.ar/sitio/contenido/inicio");
+
+    cy.fixture("eden/espectaculos").then((fixture) => {
+      fixture.Eventos.forEach((infoEvento, $index) => {
+        eden.getShowCard().eq($index).scrollIntoView();
+        eden.getShowName().eq($index).should("have.text", infoEvento.Nombre);
+        cy.wait(5);
+        eden.getShowImage().eq($index).should("have.attr", "src", infoEvento.Imagen);
+        eden.getShowImage().eq($index).trigger("mouseover");
+        eden.getShowDate().should("be.visible").and("contain.text" , infoEvento.Lugar , infoEvento.Fecha);
+      });
+    });
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,38 @@
+const Ajv = require ("ajv")
+const ajv = new Ajv();
+
+
+/**
+ * Llama al servicio acorde al primer parámetro enviado
+ * @method callService
+ * @param {string} lastUrl - última parte del endpoint que concatenará con el resto
+ * @param {string} fileName - nombre del archivo de salida 
+ */
+    
+Cypress.Commands.add("callService", (lastUrl, fileName, schema = false) => {
+    cy.request({
+        method: "GET",
+        url: "https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/" + lastUrl,
+      }).then((response)=> {
+        expect(response.status).to.eq(200);
+        cy.log(JSON.stringify(response));
+        cy.writeFile("../fixtures/eden" + fileName + ".json", response.body);
+               
+      if (!schema) {
+        cy.log("No hay validación de esquema");
+      } else {
+        cy.fixture ("schemas/" + schema).then((JsonFile) => {
+
+             
+      const validate = ajv.compile(JsonFile);
+      const valid = validate(response.body);
+      if (!valid) {
+        cy.log("Hay un error en el esquema");
+      } else {
+        cy.log("El esquema está bien");
+      }
+    });
+  }
+});
+});
+
